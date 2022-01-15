093633e8f665fbc8b6951596b3fc4de4b0ffa87d
Reza Amini <rezaaminiroyal@gmail.com>
2022-01-16 01:06:28 +0330

    Update GitLink.vue

--- src/components/GitLink.vue
+++ src/components/GitLink.vue
@@ -1,5 +1,5 @@
 <template>
-  <a href="https://github.com/samuelhorn/jamdocs" :class="size" title="Git-repository" aria-label="Jamdocs on Github">
+  <a href="https://github.com/rezaamini-ir/laravel-easypanel" :class="size" title="Git-repository" aria-label="Jamdocs on Github">
     <github-icon class="icon" />
     {{text}}
   </a>
@@ -19,7 +19,7 @@ export default {
     },
     text: {
       type: String,
-      default: 'Fork me on Github'
+      default: 'Project on Github'
     }
   }
 }
