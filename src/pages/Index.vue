821a385b19e99a061429a096f224f36a157847dc
Reza Amini <rezaaminiroyal@gmail.com>
2022-01-16 01:15:00 +0330

    Update Index.vue

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,8 +1,14 @@
 <template>
   <Layout :sidebar="true">
     <div class="content">
-      <Doc />
-      <p>Hello, form test</p>
+      <h1>{{ $static.metadata.siteName }} - {{ this.description }}</h1>
+      <nav>
+        <!-- To use other icons here, you need to import them in the Shortcut component -->
+        <Shortcut link="/getting-started" text="Introduction" icon="play-icon" />
+        <Shortcut link="/theme-configuration" text="Configuration" icon="sliders-icon" />
+        <Shortcut link="/theme-configuration#changing-colors" text="Change colors" icon="eye-icon" />
+      </nav>
+      <GitLink class="git" size="large" />
     </div>
   </Layout>
 </template>
@@ -16,11 +22,12 @@ query {
 </static-query>
 
 <script>
-import Doc from '~/templates/Doc.vue'
-
+import GitLink from '~/components/GitLink.vue'
+import Shortcut from '~/components/Shortcut.vue'
 export default {
   components: {
-    Doc
+    GitLink,
+    Shortcut
   },
   data() {
     return {
@@ -43,36 +50,29 @@ export default {
   display: flex;
   flex-direction: column;
 }
-
 h1 {
   text-align: center;
   max-width: 600px;
   margin: 1.5em auto 1.5em;
-
   @include respond-above(md) {
     max-width: 1000px;
   }
 }
-
 h2 {
   font-size: 1.4rem;
   margin: 0;
 }
-
 nav {
   display: flex;
   justify-content: space-between;
   flex-direction: column;
-
   @include respond-above(sm) {
     flex-direction: row;
   }
 }
-
 .git {
   margin: 3em 0 0;
   align-self: center;
-
   @include respond-above(md) {
     margin: 5em 0 0;
   }
