6d296f456602a7e6a9f9394c02b829d2f2526e0a
Reza Amini <rezaaminiroyal@gmail.com>
2022-01-19 22:07:43 +0330

    Change the index page

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,9 +1,29 @@
 <template>
   <Layout :sidebar="true">
     <div class="content">
-      <nav>
+      <nav style="flex-direction: column">
         <!-- To use other icons here, you need to import them in the Shortcut component -->
         <h1>EasyPanel</h1>
+        <img src="/easypanel.png" alt="">
+
+        <p style="font-size: 1.2rem">An admin panel for Laravel based on Livewire. Also, We can call it "CRUD Maker" 😃</p>
+
+        <h2>Features: </h2>
+        <ul>
+          <li class="bold">Easy to install 🚀</li>
+          <li>Multi Language</li>
+          <li>A tons of features for Fields & Inputs</li>
+          <li>RTL and LTR style</li>
+          <li>Support CKEditor and Persian Font for RTL mode</li>
+          <li class="bold">CRUD Maker GUI! 💡</li>
+          <li>Everything is customizable</li>
+          <li>A beautiful UI/UX with AdminMart template</li>
+          <li>UserProviders and Authentication classes are customizable and you can change them</li>
+          <li>Real time validation with Livewire 🚦</li>
+        </ul>
+
+        <h2 style="margin-top: 1.5rem">Demo: </h2>
+        <img src="/demo.png" alt="" style="margin-top: 1rem">
       </nav>
       <div style="display: flex; justify-content: center">
         <GitLink class="git" size="large" />
@@ -64,6 +84,15 @@ h2 {
   font-size: 1.4rem;
   margin: 0;
 }
+
+ul > li {
+  font-size: 1.2rem;
+}
+
+.bold {
+  font-weight: 800;
+}
+
 nav {
   display: flex;
   justify-content: space-between;
