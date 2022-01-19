009ae32347ad85b784f12239bb8d94feaec149e6
Reza Amini <rezaaminiroyal@gmail.com>
2022-01-19 16:05:50 +0330

    Update some styles

--- src/templates/Doc.vue
+++ src/templates/Doc.vue
@@ -42,11 +42,36 @@ export default {
 /deep/ > h2, /deep/ > h3, /deep/ > h4, /deep/ > h5, /deep/ > h6 {
   padding-top: 100px;
   margin-top: -80px;
+}
 
+/deep/ > h2::before, /deep/ > h3::before, /deep/ > h4::before {
+  content: "# ";
+}
+
+/deep/ > h2 {
   @include respond-above(md) {
-    font-size: 2rem;
+    font-size: 2.3rem;
   }
 }
+
+/deep/ > h3 {
+  @include respond-above(md) {
+    font-size: 1.5rem;
+  }
+}
+
+/deep/ > h4 {
+  @include respond-above(md) {
+    font-size: 1.1rem;
+  }
+}
+
+/deep/ > h5, /deep/ > h6 {
+  @include respond-above(md) {
+    font-size: 0.9rem;
+  }
+}
+
 
 /deep/ > p > img {
     max-width: 100%;
